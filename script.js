08cbc84748542515094e472fc9abe369b2b4b423
koppenj <joshuwakoppen@gmail.com>
2022-05-24 12:06:50 -0400

    [REFACTOR] Clean up html attribs. Work on constraint validation API implementation

--- script.js
+++ script.js
@@ -1,31 +1,54 @@
+/* eslint-disable no-console */
 /* eslint-disable no-param-reassign */
 // Use API to check inputs on form beyond what html can do
-
+/* const form = document.querySelector('form'); */
+/* const email = document.querySelector('#email');
+const zipcode = document.querySelector('#zipcode'); */
+const pwd = document.querySelector('#pwd');
+const confirmPwd = document.querySelector('#confirmPwd');
 const submit = document.querySelector('#submit-btn');
 const cancel = document.querySelector('#cancel-btn');
+const form = document.getElementById('#form');
+
+confirmPwd.addEventListener('input', () => {
+  if (confirmPwd.value !== pwd.value) {
+    confirmPwd.setCustomValidity('Must Match Password');
+    confirmPwd.reportValidity();
+  } else if (confirmPwd.value === pwd.value) {
+    console.log('Matched');
+    confirmPwd.setCustomValidity('');
+  }
+});
 
 submit.addEventListener('click', (event) => {
   event.preventDefault();
-  // check if valid. if so:
-  const formData = document.querySelectorAll('input');
-  const retrievedData = [];
-  formData.forEach((data) => {
-    retrievedData.push(data.value);
-    // eslint-disable-next-line no-console
-    console.table(retrievedData);
-  });
-  // if not:
-  // Highlight that tooltips and the first input that needs addressing
-});
 
-// I need to do some regex for the passwords and to compare the first and second inputs
+  // how to confirm ALL the form is filled?
+
+  // I dont need this. Constraint Validation API should already prevent the form from submitting
+/*   if (form.validity) {
+    const formData = document.querySelectorAll('input');
+    const retrievedData = [];
+    formData.forEach((data) => {
+      retrievedData.push(data.value);
+      // eslint-disable-next-line no-console
+      console.table(retrievedData);
+    });
+  } else {
+    console.log('check your shit');
+  } */
+});
 
 // Hey this works first shot!
 cancel.addEventListener('click', (event) => {
   event.preventDefault();
-  const theFormInputs = document.querySelectorAll('input');
+  /* const theFormInputs = document.querySelectorAll('input');
   theFormInputs.forEach((input) => {
     input.value = '';
   });
-  theFormInputs[0].focus();
+  theFormInputs[0].focus(); */
+
+  // My form isnt recognized as such because of the divs inside the form tag
+  form.reset();
+  console.log(form);
 });
