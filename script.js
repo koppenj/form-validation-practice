da7af40b8032ab985fc5383e3263b1068e6526d3
koppenj <joshuwakoppen@gmail.com>
2022-05-25 12:00:22 -0400

    [REFACTOR] Target & Validation Restructuring how JS is implementing constraint validation API

--- script.js
+++ script.js
@@ -1,14 +1,77 @@
 /* eslint-disable no-console */
 /* eslint-disable no-param-reassign */
-// Use API to check inputs on form beyond what html can do
-/* const form = document.querySelector('form'); */
-/* const email = document.querySelector('#email');
-const zipcode = document.querySelector('#zipcode'); */
+
+const email = document.querySelector('#email');
+const country = document.querySelector('#select-country');
+const zipcode = document.querySelector('#zipcode');
 const pwd = document.querySelector('#pwd');
 const confirmPwd = document.querySelector('#confirmPwd');
 const submit = document.querySelector('#submit-btn');
 const cancel = document.querySelector('#cancel-btn');
-const form = document.getElementById('#form');
+const form = document.querySelector('#form');
+const formInputs = document.querySelectorAll('input');
+
+submit.addEventListener('click', (event) => {
+  event.preventDefault();
+  if (form.checkValidity()) {
+    console.log('success');
+    formInputs.forEach((input) => {
+      input.value = '';
+    });
+  } else {
+    console.log('NOPE');
+    Array.from(formInputs);
+    const failedInput = form.map((input) => input.checkValidity());
+    console.log(failedInput[0]);
+  }
+});
+
+cancel.addEventListener('click', (event) => {
+  event.preventDefault();
+  formInputs.forEach((input) => {
+    input.value = '';
+  });
+  formInputs[0].focus();
+});
+
+// This next section could probably be a loop to set all the listeners and then..
+// Set the custom validity messages to an interpolation depending on what element needs the message.
+
+email.addEventListener('keyup', () => {
+  if (email.validity.typeMismatch) {
+    email.setCustomValidity('I am expecting an e-mail address!');
+    email.reportValidity();
+  } else {
+    email.setCustomValidity('');
+  }
+});
+
+country.addEventListener('blur', () => {
+  if (country.validity.typeMismatch) {
+    country.setCustomValidity('Select One');
+    country.reportValidity();
+  } else {
+    country.setCustomValidity('');
+  }
+});
+
+zipcode.addEventListener('input', () => {
+  if (zipcode.validity.patternMismatch) {
+    zipcode.setCustomValidity('Incorrect Value. Requires 5 numbers.');
+    zipcode.reportValidity();
+  } else {
+    zipcode.setCustomValidity('');
+  }
+});
+
+pwd.addEventListener('blur', () => {
+  if (pwd.validity.typeMismatch) {
+    pwd.setCustomValidity('See Tip For Details On Setting Password');
+    pwd.reportValidity();
+  } else {
+    pwd.setCustomValidity('');
+  }
+});
 
 confirmPwd.addEventListener('input', () => {
   if (confirmPwd.value !== pwd.value) {
@@ -18,37 +81,4 @@ confirmPwd.addEventListener('input', () => {
     console.log('Matched');
     confirmPwd.setCustomValidity('');
   }
-});
-
-submit.addEventListener('click', (event) => {
-  event.preventDefault();
-
-  // how to confirm ALL the form is filled?
-
-  // I dont need this. Constraint Validation API should already prevent the form from submitting
-/*   if (form.validity) {
-    const formData = document.querySelectorAll('input');
-    const retrievedData = [];
-    formData.forEach((data) => {
-      retrievedData.push(data.value);
-      // eslint-disable-next-line no-console
-      console.table(retrievedData);
-    });
-  } else {
-    console.log('check your shit');
-  } */
-});
-
-// Hey this works first shot!
-cancel.addEventListener('click', (event) => {
-  event.preventDefault();
-  /* const theFormInputs = document.querySelectorAll('input');
-  theFormInputs.forEach((input) => {
-    input.value = '';
-  });
-  theFormInputs[0].focus(); */
-
-  // My form isnt recognized as such because of the divs inside the form tag
-  form.reset();
-  console.log(form);
 });
